237ca0087ca0ba595bfea9a3a47fb8e0b0ebbadf
erajshekhar <rajshekhar.rao@gmail.com>
2016-02-01 14:24:02 -0500

    new variables added

--- test.c
+++ test.c
@@ -2,6 +2,9 @@
 main() {
   int i = 1;
   int j = 1;
+  int j1 = 1;
+  int j2 = 1;
+  int j3 = 1;
   printf ("Hello Raj: Test %d\n", i);
   printf ("Hello Raj: Test %d\n", i);
   printf ("Hello Raj: Test %d\n", i);
